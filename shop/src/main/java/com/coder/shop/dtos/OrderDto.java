6237ea24522ed32809f80d65791bb0966e60ec1b
Aung Nanda Oo <aungnandaoo.425@gmail.com>
2025-12-26 07:01:31 -0500

    orderDto impls

--- shop/src/main/java/com/coder/shop/dtos/OrderDto.java
+++ shop/src/main/java/com/coder/shop/dtos/OrderDto.java
@@ -1,9 +1,6 @@
 package com.coder.shop.dtos;
 
-import java.util.ArrayList;
 import java.util.List;
-
-import com.coder.shop.models.OrderItem;
 
 import lombok.Data;
 
@@ -11,20 +8,4 @@ import lombok.Data;
 public class OrderDto {
     private List<OrderItemDto> items;
     private double total;
-
-    public double getTotal() {
-        total = 0;
-        for(OrderItemDto item : items) {
-            total += item.getCount() * item.getProduct().getPrice();
-        }
-        return total;
-    }
-
-    public List<OrderItem> getOrderItems() {
-        List<OrderItem> oItems = new ArrayList<OrderItem>();
-        for (OrderItemDto item: items) {
-            oItems.add(item.getOrderItem());
-        }
-        return oItems;
-    }
 }
