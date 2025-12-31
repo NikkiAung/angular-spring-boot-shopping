6121d8b2ea0fffa5339090d123ff244c721e83a6
Aung Nanda Oo <aungnandaoo.425@gmail.com>
2025-12-31 06:39:39 -0800

    cartpage

--- shoppy/src/app/cartpage/cartpage.ts
+++ shoppy/src/app/cartpage/cartpage.ts
@@ -1,16 +1,28 @@
 import { Component } from '@angular/core';
 import { DBProductModel } from '../models/dbproductmodel';
+import { ChangeService } from '../change-service';
+import { Apiservice } from '../apiservice';
+import { CommonModule } from '@angular/common';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-cartpage',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './cartpage.html',
   styleUrl: './cartpage.css',
 })
 export class Cartpage {
   products: DBProductModel[] = [];
-  constructor() {
-    this.products = JSON.parse(localStorage.getItem('products') || '');
+  constructor(
+    private apiService: Apiservice,
+    private changeService: ChangeService,
+    private rotuer: Router
+  ) {
+    this.products = JSON.parse(localStorage.getItem('products') || '[]');
+  }
+
+  ngOnInit() {
+    this.changeService.updateProductCount(this.products.length);
   }
 
   reduceQuantity(product: DBProductModel) {
@@ -25,6 +37,7 @@ export class Cartpage {
   removeProduct(product: DBProductModel) {
     this.products = this.products.filter((p) => p.id !== product.id);
     this.uploadLocalStorage();
+    this.changeService.updateProductCount(this.products.length);
   }
 
   increaseQuantity(product: DBProductModel) {
@@ -33,6 +46,38 @@ export class Cartpage {
   }
 
   uploadLocalStorage() {
-    localStorage.setItem('product', JSON.stringify(this.products));
+    localStorage.setItem('products', JSON.stringify(this.products));
+  }
+
+  getTotalPrice(): number {
+    return this.products.reduce((total, product) => total + product.price * product.count, 0);
+  }
+
+  checkOut() {
+    let items = this.products.map((product) => ({
+      id: product.id,
+      count: product.count,
+    }));
+
+    let orderData = {
+      items: items,
+      total: this.getTotalPrice(),
+    };
+
+    this.apiService.sendOrder(orderData).subscribe({
+      next: (response) => {
+        this.clearCart();
+        this.rotuer.navigate(['/history']);
+      },
+      error: (error) => {
+        console.log(error);
+      },
+    });
+  }
+
+  clearCart() {
+    this.products = [];
+    this.uploadLocalStorage();
+    this.changeService.updateProductCount(0);
   }
 }
