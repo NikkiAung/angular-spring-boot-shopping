3eb717f5f7ed39e7580311455aa12a3b4e6801d4
Aung Nanda Oo <aungnandaoo.425@gmail.com>
2026-01-10 18:15:12 -0800

    orderItem

--- shop/src/main/java/com/coder/shop/models/OrderItem.java
+++ shop/src/main/java/com/coder/shop/models/OrderItem.java
@@ -1,10 +1,11 @@
 package com.coder.shop.models;
 
+import java.util.List;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 import lombok.Data;
 
@@ -16,7 +17,8 @@ public class OrderItem {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private int count;
+    private String name;
     private double price;
-    @OneToOne
-    private Product product;
+    private List<String> images;    
+    private Long productId;
 }
